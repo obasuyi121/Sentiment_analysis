0a290042334d5ab1a4f16c2176c02754acde43ee
obasuyi121 <ivonobasuyi@gmail.com>
2023-03-19 12:42:07 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+app = Flask(__name__)
+analyzer = SentimentIntensityAnalyzer()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    text = request.form['text']
+    sentiment = analyze_sentiment(text)
+    return {"sentiment": sentiment}
+
+
+def analyze_sentiment(text):
+    sentiment_scores = analyzer.polarity_scores(text)
+    compound_score = sentiment_scores['compound']
+
+    if compound_score >= 0.05:
+        sentiment = "Positive"
+    elif compound_score <= -0.05:
+        sentiment = "Negative"
+    else:
+        sentiment = "Neutral"
+
+    return sentiment
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
